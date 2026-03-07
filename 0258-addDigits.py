4e217716441b937abaf22e6a6ba0c6efb75f12bf
Rahul Lumbhani <229122485+Rahul-134@users.noreply.github.com>
2026-03-07 21:46:46 +0530

    Solved Q258-addDigits using loop and recursion

---
+++ 0258-addDigits.py
@@ -0,0 +1,10 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        s = 0
+        while num != 0:
+            s += num%10
+            num = num // 10
+        return s if s<10 else self.addDigits(s)
+
+c = Solution()
+print(c.addDigits(0))
