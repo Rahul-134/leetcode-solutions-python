21e1c115cc6f14d76aad5e38ead50911ef2cffd8
Rahul Lumbhani <229122485+Rahul-134@users.noreply.github.com>
2026-03-09 22:16:24 +0530

    Updated Q67

--- 0067-add-binary.py
+++ 0067-add-binary.py
@@ -1,22 +1,24 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        # if a == "0" and b == "0":
-        #     return "0"
-        # n1, n2 = 0, 0
-        # a, b = a[::-1], b[::-1]
-        # for i in range(len(a)):
-        #     n1 += ((int(a[i])) * (2 ** i))
-        # for i in range(len(b)):
-        #     n2 += ((int(b[i])) * (2 ** i))
-        # result = n1 + n2
-        # s = ""
-        # while result > 0:
-        #     s += str(result%2)
-        #     result = result // 2
-        # return s[::-1]
-        a1 = int(a,2)
-        a2 = int(b,2)
-        return bin(a1+a2)[2:]
+        if a == "0" and b == "0":
+            return "0"
+        n1, n2 = 0, 0
+        a, b = a[::-1], b[::-1]
+        for i in range(len(a)):
+            n1 += ((int(a[i])) * (2 ** i))
+        for i in range(len(b)):
+            n2 += ((int(b[i])) * (2 ** i))
+        result = n1 + n2
+        s = ""
+        while result > 0:
+            s += str(result%2)
+            result = result // 2
+        return s[::-1]
+
+        # Alternate solution:
+        # a1 = int(a,2)
+        # a2 = int(b,2)
+        # return bin(a1+a2)[2:]
 
 
 c = Solution()
