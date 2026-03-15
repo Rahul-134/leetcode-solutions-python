75ba3824ad0eb095f45610cd81cf2eae6076451e
Rahul Lumbhani <229122485+Rahul-134@users.noreply.github.com>
2026-03-15 22:30:55 +0530

    Solved Q21 using loops and if-else conditions

---
+++ 0021-merge-two-sorted-lists.py
@@ -0,0 +1,33 @@
+from typing import Optional
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        tail = dummy
+        
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+        
+        if list1:
+            tail.next = list1
+        elif list2:
+            tail.next = list2
+        
+        return dummy.next
+
+
+c = Solution()
+l1 = ListNode(1, ListNode(2, ListNode(4)))
+l2 = ListNode(1, ListNode(3, ListNode(4)))
+print(c.mergeTwoLists(l1, l2))
