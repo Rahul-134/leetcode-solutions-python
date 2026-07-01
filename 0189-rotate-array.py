4fc58fb992b88a27d935897317f92b8d1e837e58
Rahul Lumbhani <229122485+Rahul-134@users.noreply.github.com>
2026-07-01 19:01:01 +0530

    Solved Q189 using insert() method of python list

---
+++ 0189-rotate-array.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(k):
+            nums.insert(0, nums.pop())
+        print(nums)  # -> just to see the output
+
+c = Solution()
+c.rotate([1, 2, 3, 4, 5, 6, 7], 3)
